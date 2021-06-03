48ce25b91bc979f8cea715db75e862537fa0a4a4
Raghda  Muayad <therose000777@gmail.com>
2021-06-03 15:37:11 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class Form extends React.Component {
-  
+
   render() {
     return (
       <div>
       <form >
-      <input 
-      type="text" 
+      <input
+      type="text"
       name="firstName"
-      onChange={event => this.props.handleChange(event)} 
+      onChange={event => this.props.handleChange(event)}
       value={this.props.formData.firstName} />
-      <input type="text" 
+      <input type="text"
       name="lastName"
-      onChange={event => this.props.handleChange(event)} 
+      onChange={event => this.props.handleChange(event)}
       value={this.props.formData.lastName} />
       <input type="submit" />
       </form>
